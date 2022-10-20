52f09795e094a8a4ef0c7325742b86a44c14be71
Jefferson Lima <jeffyuri@hotmail.com>
2022-10-20 10:06:41 -0300

    Create class Manipulador

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from gi.repository import Gtk
 builder = Gtk.Builder()
 builder.add_from_file("ui_dig_ver.glade")
 
+class Manipulador:
+    def on_main_window_destroy(self, window):
+        Gtk.main_quit()
+
+
+builder.connect_signals(Manipulador())
 window = builder.get_object('main_window')
 window.show_all()
 Gtk.main()
